5960e14144cebdb141947f5d8d3cad83413d45d4
xnzale <bdnzale@gmail.com>
2019-09-26 22:11:18 +0000

    ListerEmployeController

--- src/main/java/com/devweb/rh/controller/EmployeController.java
+++ src/main/java/com/devweb/rh/controller/EmployeController.java
@@ -1,12 +1,26 @@
 package com.devweb.rh.controller;
 
+import com.devweb.rh.model.User;
+import com.devweb.rh.repository.EmployeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
+@RequestMapping(value = "/employe")
 public class EmployeController {
     @Autowired
-    private EmployeController employeRepository;
+    private EmployeRepository employeRepository;
+    @GetMapping(value = "/liste")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public List<User> liste(){
+        return employeRepository.findAll();
+    }
+
 }
